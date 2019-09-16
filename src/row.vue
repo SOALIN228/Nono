bec94b32e4563163ca7b1667eee342e2c3ef3f4a
lml <soalin228@163.com>
2019-09-16 15:35:00 +0800

    添加栅格

---
+++ src/row.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NRow',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>

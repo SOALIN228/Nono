8ca8ad879fd4930bce7ae20cc6da1f6ce1a4e5c2
lml <soalin228@163.com>
2019-09-17 23:14:35 +0800

    添加栅格响应式，默认样式为phone

--- src/row.vue
+++ src/row.vue
@@ -40,12 +40,16 @@ export default {
 <style lang="scss" scoped>
   .row {
     display: flex;
+    flex-wrap: wrap;
+
     &.align-left {
       justify-content: flex-start;
     }
+
     &.align-right {
       justify-content: flex-end;
     }
+
     &.align-center {
       justify-content: center;
     }

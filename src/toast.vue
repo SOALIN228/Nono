62b93899c51d17c770116938eebd1edd3665262f
lml <soalin228@163.com>
2019-09-20 12:14:58 +0800

    添加toast 完善动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="toast" :class="`position-${position}`" ref="wrapper">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
+  <div class="wrapper" :class="toastClasses">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
+      <div class="line" ref="line"></div>
+      <span class="close" v-if="closeButton" @click="onClickClose">
+        {{closeButton.text}}
+      </span>
     </div>
-    <div class="line" ref="line"></div>
-    <span class="close" v-if="closeButton" @click="onClickClose">
-      {{closeButton.text}}
-    </span>
   </div>
 </template>
 
@@ -44,9 +46,6 @@ export default {
       }
     }
   },
-  data () {
-    return {}
-  },
   mounted () {
     if (this.autoClose) {
       setTimeout(() => {
@@ -54,8 +53,15 @@ export default {
       }, this.autoCloseDelay * 1000)
     }
     this.$nextTick(() => { // $nextTick 在dom 更新完才会被调用，因为vue 更新dom 是异步的
-      this.$refs.line.style.height = `${this.$refs.wrapper.getBoundingClientRect().height}px`
+      this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
     })
+  },
+  computed: {
+    toastClasses () {
+      return {
+        [`position-${this.position}`]: true
+      }
+    }
   },
   methods: {
     close () {
@@ -80,16 +86,77 @@ export default {
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
-  @keyframes fade-in {
-    0% {opacity: 0; transform: translateY(100%);}
-    100% {opacity: 1;transform: translateY(0%);}
+  @keyframes slide-up {
+    0% {
+      opacity: 0;
+      transform: translateY(100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
   }
+
+  @keyframes slide-down {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .wrapper {
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    $animation-duration: 300ms;
+
+    &.position-top {
+      top: 0;
+
+      .toast {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        animation: slide-down $animation-duration;
+      }
+    }
+
+    &.position-bottom {
+      bottom: 0;
+
+      .toast {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        animation: slide-up $animation-duration;
+      }
+    }
+
+    &.position-middle {
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%);
+
+      .toast {
+        animation: fade-in $animation-duration;
+      }
+    }
+  }
+
   .toast {
-    animation: fade-in 1s;
     font-size: $font-size;
     min-height: $toast-min-height;
     line-height: 1.8;
-    position: fixed;
     left: 50%;
     display: flex;
     color: white;
@@ -112,21 +179,6 @@ export default {
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
-    }
-
-    &.position-top {
-      top: 0;
-      transform: translateX(-50%);
-    }
-
-    &.position-bottom {
-      bottom: 0;
-      transform: translateX(-50%);
-    }
-
-    &.position-middle {
-      top: 50%;
-      transform: translate(-50%, -50%);
     }
   }
 </style>

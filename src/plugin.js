8fd2617b251cfb0ae18a574edc0b2abdd07e67ca
lml <soalin228@163.com>
2019-09-20 10:46:58 +0800

    添加toast 实现只能出现一个

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,23 @@
 import Toast from './toast'
 
+let currentToast
+
 export default {
   install (Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message] // 将内容传入插槽中
-      toast.$mount() // 手动开启编译
-      document.body.appendChild(toast.$el) // 挂载到页面
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({ Vue, message, propsData: toastOptions })
     }
   }
+}
+
+function createToast ({ Vue, message, propsData }) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({ propsData })
+  toast.$slots.default = [message] // 将内容传入插槽中
+  toast.$mount()  // 手动开启编译
+  document.body.appendChild(toast.$el) // 挂载到页面
+  return toast
 }

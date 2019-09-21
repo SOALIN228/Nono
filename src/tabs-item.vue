ab54d62f9ee2d5e55ce253fc2aaaaf06c255b644
lml <soalin228@163.com>
2019-09-21 14:46:08 +0800

    添加tabs 组件切换

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx">
+  <div class="tabs-item" :class="classes" @click="xxx" >
     <slot></slot>
   </div>
 </template>
@@ -23,9 +23,15 @@ export default {
       active: false
     }
   },
+  computed: {
+    classes () {
+      return {
+        active: this.active
+      }
+    }
+  },
   created () {
     this.eventBus.$on('update:selected', (name) => {
-      console.log(name)
       this.active = name === this.name
     })
   },
@@ -39,6 +45,10 @@ export default {
 
 <style lang="scss" scoped>
   .tabs-item {
-
+    flex-shrink: 0;
+    padding: 0 1em;
+    &.active {
+      background: red;
+    }
   }
 </style>

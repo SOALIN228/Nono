18f6df2ffd6974bfa69ef6a6462f8a71178b55bd
lml <soalin228@163.com>
2019-09-25 14:22:29 +0800

    添加默认selected

--- src/collapse.vue
+++ src/collapse.vue
@@ -13,6 +13,9 @@ export default {
     single: {
       type: Boolean,
       default: false
+    },
+    selected: {
+      type: String,
     }
   },
   data () {
@@ -27,9 +30,15 @@ export default {
       }
     } else {
       return {
-        eventBus: null
+        eventBus: this.eventBus
       }
     }
+  },
+  mounted () {
+    this.eventBus.$emit('update:selected', this.selected)
+    this.eventBus.$on('update:selected', (name) => { // 将变化的selected 传递给外界
+      this.$emit('update:selected', name)
+    })
   }
 }
 </script>
